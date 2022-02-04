5333a40a9ab6bf2bcf29010b8b5ff1de1c954af1
Scott-fo <scottfo@live.com>
2022-02-04 05:54:57 +0000

    mobile style

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,10 +7,10 @@ function Hero() {
       
         <div className='flex justify-between items-center border-y shadow py-10 lg:py-0 bg-header-banner bg-cover bg-center h-96'>
         <div className='flex justify-between items-center bg-black/30 h-96 bg-cover w-full'>
-            <div className='px-10 space-y-5 bg-black-' >
-                <h1 className='text-6xl max-w-xl font-serif text-white'>Welcome to my blog!</h1>
-                <h2 className='text-xl max-w-xl font-serif text-white'> This page contains information about me, in-progress and completed projects, and thoughts on interesting things that I am reading.</h2>
-                <h3 className='text-l font-serif text-yellow-500'>- Scott Foster</h3>
+            <div className='px-10 space-y-3 bg-black-' >
+                <h1 className='text-6xl max-w-xl font-serif text-white pt-2'>Welcome to my blog!</h1>
+                <h2 className='text-xl max-w-xl font-serif text-white mb-0 pb-0 '> This page contains information about me, in-progress and completed projects, and thoughts on interesting things that I am reading.</h2>
+                <p className='text-l font-serif text-yellow-500 mt-0'>- Scott Foster</p>
             </div>
         </div>
       </div>
