3c3797ca78ca9f00e3270bc53e715bd4889af4f3
Scott-fo <scottfo@live.com>
2022-02-04 05:12:18 +0000

    formatting improvements

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -42,15 +42,16 @@ function Post({ post }: Props) {
 
         <article className="max-w-3xl mx-auto p-5">
             <img src={urlFor(post.mainImage).url()!} alt="" />
-            <h1 className="text-3xl mt-10 mb-3 mx-auto">{post.title}</h1>
             <h2 className="font-extralight text-sm">Hero Image Credit: {post.imageCredit}</h2>
+            <h1 className="text-3xl mt-10 mb-3 mx-auto text-yellow-500 font-bold">{post.title}</h1>
+            
             <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
 
-            <div>
-                <img className="h-10 w-10 rounded-full" src={urlFor(post.author.image).url()!} alt="" />
-                <p className="font-extralight text-sm">Blog post by: <span className="text-yellow-500">{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString()}</p>
+            <div className="flex items-center space-x-2">
+                <img className="h-10 w-10 rounded-full mb-1" src={urlFor(post.author.image).url()!} alt="" />
+                <p className="font-extralight text-sm mb-2">Blog post by: <span className="text-yellow-500">{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString()}</p>
             </div>
-            <div>
+            <div className="mt-10">
                 <PortableText 
                 className=""
                 dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
@@ -61,10 +62,10 @@ function Post({ post }: Props) {
                         <h1 className="text-2xl font-bold my-5" {...props}/>
                     ),
                     h2: (props: any) => (
-                        <h1 className="text-2xl font-bold my-5"{...props}/>
+                        <h1 className="text-xl font-bold my-5"{...props}/>
                     ),
                     li: ({ children }: any) => (
-                        <li className="text-2xl font-bold my-5">{children}</li>
+                        <li className="ml-5 list-disc">{children}</li>
                     ),
                     link: ({href, children}: any) => (
                         <a href={href} className="text-blue-500 hover:underline">{children}</a>
@@ -73,6 +74,7 @@ function Post({ post }: Props) {
                 />
             </div>
         </article>
+        <h2 className="my-5 max-w-3xl mx-auto p-5 text-center font-bold text-lg">Share your thoughts below!</h2>
         <hr className="max-w-lg my-5 mx-auto border border-yellow-500"/>
 
         {submitted ? (
@@ -126,7 +128,7 @@ function Post({ post }: Props) {
 
 
         <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
-            <h3 className="text-4xl">Share your thoughts below!</h3>
+            <h3 className="text-4xl">Comments</h3>
             <hr className="pb-2"/>
             {post.comments.map((comment) => (
                 <div key={comment._id}>
