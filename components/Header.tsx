98b21fdf1d00e15e625ce6fb7d1e2a21937a29f9
Scott-fo <scottfo@live.com>
2022-02-14 03:20:54 +0000

    temporarily removed secondary site link

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
         <div className="flex items-center mx-auto space-x-5">
             <div className = "hidden md:inline-flex items-center space-x-5">
                 <Link href="/" ><h3 className = "cursor-pointer">Home</h3></Link>
-                <Link href="https://scott-foster.netlify.app/"><h3 className = "cursor-pointer">About</h3></Link>
+                {/* <Link href="https://scott-foster.netlify.app/"><h3 className = "cursor-pointer">About</h3></Link> */}
                 <Link href="https://www.linkedin.com/in/scottfo/"><h3 className = "cursor-pointer">Contact</h3></Link>
                 <Link href="https://github.com/Scott-fo"><h3 className='text-black bg-yellow-500 px-4 py-1 rounded-full cursor-pointer'>GitHub</h3></Link>
             </div>
