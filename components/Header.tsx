bd4186684c3ea4ae984c11d239fd5f8049eba5c9
Scott-fo <scottfo@live.com>
2022-02-14 03:03:34 +0000

    sticky header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className = "flex justify-between max-w-7xl mx-auto md:p-5">
+    <header className = "flex justify-between max-w-7xl mx-auto md:p-5 sticky top-0 bg-white">
         <div className="flex items-center mx-auto space-x-5">
             <div className = "hidden md:inline-flex items-center space-x-5">
                 <Link href="/" ><h3 className = "cursor-pointer">Home</h3></Link>
